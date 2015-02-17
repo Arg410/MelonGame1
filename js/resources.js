edf8622c0bc1bce17d99a88758c4f3ada08c80c8
Arg410 <geo-aar@2015veg-17.local>
2015-02-17 10:50:42 -0600

    Added things

--- js/resources.js
+++ js/resources.js
@@ -5,9 +5,15 @@ game.resources = [
    */
   // our level tileset
   {name: "area01_level_tiles",  type:"image", src: "data/img/map/area01_level_tiles.png"},
- 
-  /* 
-   * Maps. 
+
+  {name: "gripe_run_right", type:"image", src: "data/img/sprite/gripe_run_right.png"},
+
+  {name: "spinning_coin_gold",  type:"image", src: "data/img/sprite/spinning_coin_gold.png"},
+  // our enemty entity
+  {name: "wheelie_right",       type:"image", src: "data/img/sprite/wheelie_right.png"},
+
+  /*
+   * Maps.
    */
   {name: "area01", type: "tmx", src: "data/map/area01.tmx"}
 ];
